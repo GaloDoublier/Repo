f17ca16981e84b5e5e9d13ce988c03c29a3b6287
GaloDoublier <81277730+GaloDoublier@users.noreply.github.com>
2022-06-11 20:59:00 -0300

    gymorgProject
    pantalla principal y de agregar usuario terminadas falta database y perfil

---
+++ GIMNASIO/gimnasio/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace gimnasio.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ GIMNASIO/gimnasio/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function agregarEjercicio(){
+    let divEjercicios = document.getElementById("ejercicios");
+    let ejercicio = document.getElementById("ejercicio").value;
+    let peso = document.getElementById("peso").value;
+    let reps = document.getElementById("reps").value;
+    let datos = ejercicio + " " + peso + "kg" +" " + reps + " reps";
+    if(ejercicio != "" || peso != "" || reps != ""){
+        let li = document.createElement("li");
+        li.innerHTML = datos;
+        li.setAttribute("class", "list-group-item");
+        divEjercicios.appendChild(li);
+    }
+    else{
+        alert("Debe ingresar un ejercicio");
+    }
+}
