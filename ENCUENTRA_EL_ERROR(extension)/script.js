d196c5ee03a9b4806376ae71c14704ea8f68f362
GaloDoublier <81277730+GaloDoublier@users.noreply.github.com>
2022-06-11 11:57:05 -0300

    VERSION 1.0.1

---
+++ ENCUENTRA_EL_ERROR(extension)/script.js
@@ -0,0 +1,111 @@
+let divTexto = document.getElementById("texto");
+let random = Math.floor(Math.random() *86)
+let letraElegidaN = "";
+let letraElegidaE = "";
+var dificultad = "no data";
+var errores= 0;
+var erroresPermitidos= 0;
+
+document.getElementById("facil").addEventListener("click", ()=>{mostrarDificultad("facil");});
+document.getElementById("medio").addEventListener("click", ()=>{mostrarDificultad("medio");})
+document.getElementById("dificil").addEventListener("click", ()=>{mostrarDificultad("dificil");})
+document.getElementById("empezemos").addEventListener("click", empezarJuego)
+
+function mostrarDificultad(dificultad){
+  divDificultad=document.getElementById("dificultad");
+  if(dificultad=="facil"){
+    divDificultad.innerHTML=" <select id='rta' class='form-select selec'><option>8 y &</option><option>8 y B</option></select>"
+  erroresPermitidos=3;
+  }
+  else{
+    if(dificultad=="medio"){
+      divDificultad.innerHTML="<select id='rta' class='form-select selec'><option>I y 1</option> <option>5 y S</option> </select>"
+      erroresPermitidos=2;
+    }
+    else{
+      divDificultad.innerHTML="<select id='rta' class='form-select selec'><option>l y I</option></select>"
+      erroresPermitidos=1;
+    }
+  }
+}
+function empezarJuego(){
+  dificultad=guardarDatos();
+  contenedor= document.getElementsByClassName("container")[0];
+  contenedor.remove();
+  dificultad = dificultad.split(" y ")
+  letraElegidaN=dificultad[0];
+  letraElegidaE=dificultad[1];
+  jugar();
+  
+}
+function guardarDatos(){
+  rta=document.getElementById("rta").value
+  return rta;
+}
+
+function verRTA(letra){
+  if(letra=="escondida"){
+    ganaron();
+  }else{
+    errores += 1;
+    document.getElementById("errores").innerText="errores: "+errores
+    verSiPerdio();
+  }
+}
+function ganaron(){
+  contenedor= document.getElementsByClassName("container")[0];
+  contenedor.remove();
+  p=document.getElementById("errores")
+  p.innerText="Ganaste!"
+  p.style.color="green"
+  p.style.fontSize="80px"
+  div = document.getElementById("volverAjugar")
+  boton=document.createElement("button" )
+  boton.setAttribute("class", "btn btn-warning")
+  boton.textContent="volver a jugar"
+  boton.addEventListener("click", () =>{
+    window.location.href="popup2.html"
+  })
+  div.appendChild(boton)  
+  
+}
+
+function verSiPerdio(){
+  if(errores==erroresPermitidos){
+    contenedor= document.getElementsByClassName("container")[0];
+    contenedor.remove();
+    p = document.getElementById("errores")
+    p.innerText="Perdiste :("
+    p.style.fontSize="80px"
+    div = document.getElementById("volverAjugar")
+  boton=document.createElement("button" )
+  boton.setAttribute("class", "btn btn-warning")
+  boton.textContent="volver a jugar"
+  boton.addEventListener("click", () =>{
+    window.location.href="popup2.html"
+  })
+  div.appendChild(boton)  
+  }
+}
+
+function jugar(){
+  for(let i=0;i<=86;i++){
+    if(i==random){
+    let div = document.createElement("div")
+    div.setAttribute("class", "col")
+    let a = document.createElement("a")
+    a.innerText=" "+ letraElegidaE + " "
+    a.addEventListener("click", ()=>{verRTA("escondida")})
+    div.appendChild(a)
+    divTexto.appendChild(div)
+    }
+    let divParaP = document.createElement("div")
+    divParaP.setAttribute("class" , "col")
+    let a = document.createElement("a")
+    a.innerText=" "+ letraElegidaN + " "
+    a.addEventListener("click", ()=>{verRTA("normal")})
+    divParaP.appendChild(a)
+    divTexto.appendChild(divParaP)
+}
+
+}
